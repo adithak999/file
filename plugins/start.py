b425d77ab9c7b47fc2d4f1213ad3ed349a249147
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-22 23:18:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,6 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
-from MKN.database import add_user, getid
 
 
 #=====================================================================================##
