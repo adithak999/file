ceeb283c396d48d24e852412cffbff2e0e2e817a
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-22 22:50:27 +0530

    Update database.py

--- MKN/database.py
+++ MKN/database.py
@@ -1,4 +1,5 @@
 import pymongo
+import motor.motor_asyncio
 from config import DB_URI
 
 
@@ -6,7 +7,11 @@ DB_NAME = os.environ.get("DB_NAME","cloud19")
 mongo = pymongo.MongoClient(DB_URI)
 db = mongo[DB_NAME]
 dbcol = db["user"]
-  
+ 
+
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
 
     def insert(chat_id):
             user_id = int(chat_id)
