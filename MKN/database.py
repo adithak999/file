b8c5dc4e02139dbf1dac93853bf318c84512b452
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-04-22 22:45:34 +0530

    Create database.py

---
+++ MKN/database.py
@@ -0,0 +1,40 @@
+import pymongo
+import motor.motor_asyncio
+from config import DB_URI
+
+
+DB_NAME = os.environ.get("DB_NAME","cloud19")                              
+mongo = pymongo.MongoClient(DB_URI)
+db = mongo[DB_NAME]
+dbcol = db["user"]
+   
+class Database:
+
+
+    def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"file_id":None}
+            try:
+            	dbcol.insert_one(user_det)
+            except:
+            	pass
+
+    def getid():
+        values = []
+        for key  in dbcol.find():
+             id = key["_id"]
+             values.append((id)) 
+        return values 
+     
+
+
+
+
+
+
+
+
+
+
+
+
