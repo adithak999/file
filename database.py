4bfe4cd91084a512fd779b446a92998f37b5bc15
Mr muha <91768693+MuhaKn@users.noreply.github.com>
2022-05-01 09:59:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import motor.motor_asyncio
 import pymongo
 import os
 
@@ -7,12 +6,6 @@ DB_URL = os.environ.get("DB_URL","mongodb+srv://VC:VC@cluster0.zyphn.mongodb.net
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
-
-def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
-        self.grp = self.db.groups
 
 def insert(chat_id):
             user_id = int(chat_id)
@@ -29,10 +22,4 @@ def getid():
          values.append((id)) 
     return values
 
-async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
 
-async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
