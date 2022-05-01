df5d6448febed1a6236f3ea56594e84a12e575fc
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-01 07:50:28 +0530

    Create broadcast.py

---
+++ plugins/broadcast.py
@@ -0,0 +1,16 @@
+from pyrogram import Client ,filters
+from database import getid
+from config import ADMINS
+
+@Client.on_message(filters.private & filters.user(ADMINS) & filters.command(["broadcast"]))
+async def broadcast(bot, message):
+ if (message.reply_to_message):
+   ms = await message.reply_text("Geting All ids from database ...........")
+   ids = getid()
+   tot = len(ids)
+   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   for id in ids:
+     try:
+     	await message.reply_to_message.copy(id)
+     except:
+     	pass
